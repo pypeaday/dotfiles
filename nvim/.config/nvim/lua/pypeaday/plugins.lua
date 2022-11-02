1e1293a3825b9a405bdb190110912ff1e934d933
Pypeaday <pypeaday@pm.me>
2022-11-02 06:05:05 -0500

    add cmp path

--- nvim/.config/nvim/lua/pypeaday/plugins.lua
+++ nvim/.config/nvim/lua/pypeaday/plugins.lua
@@ -337,6 +337,7 @@ use({
     -- 'honza/vim-snippets',
     'L3MON4D3/LuaSnip',
     'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lsp-signature-help',
