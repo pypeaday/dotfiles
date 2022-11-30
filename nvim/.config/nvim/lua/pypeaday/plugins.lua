d00fda7516317e7c91bf39f26cf78b82521b8cea
Pypeaday <pypeaday@pm.me>
2022-11-30 13:44:09 -0600

    gitsigns in vim

--- nvim/.config/nvim/lua/pypeaday/plugins.lua
+++ nvim/.config/nvim/lua/pypeaday/plugins.lua
@@ -267,15 +267,15 @@ use({
 use({
   'lewis6991/gitsigns.nvim',
   requires = 'nvim-lua/plenary.nvim',
-  -- config = function()
-  --   require('gitsigns').setup({
-  --     sign_priority = 20,
-  --     on_attach = function(bufnr)
-  --       vim.keymap.set('n', ']h', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true, buffer = bufnr })
-  --       vim.keymap.set('n', '[h', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true, buffer = bufnr })
-  --     end,
-  --   })
-  -- end,
+  config = function()
+    require('gitsigns').setup({
+      -- sign_priority = 20,
+      -- on_attach = function(bufnr)
+      --   vim.keymap.set('n', ']h', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true, buffer = bufnr })
+      --   vim.keymap.set('n', '[h', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true, buffer = bufnr })
+      -- end,
+    })
+  end,
 })
 
 use({
