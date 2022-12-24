7f1042baec24892d21f32c48d76c9ff580d285e3
Pypeaday <pypeaday@pm.me>
2022-12-24 14:25:53 -0600

    comment out winbar... not ready to delete yet

--- nvim/.config/nvim/lua/pypeaday/plugins.lua
+++ nvim/.config/nvim/lua/pypeaday/plugins.lua
@@ -374,17 +374,17 @@ use({
 
 use({'ThePrimeagen/vim-be-good'})
 
-use({ 
-  'fgheng/winbar.nvim',
-  config = function()
-    require('pypeaday.plugins.winbar')
-  end,
-  requires = {
-    "SmiteshP/nvim-navic",
-    "neovim/nvim-lspconfig",
-    "nvim-tree/nvim-web-devicons"
-  },
-}) 
+-- use({ 
+--   'fgheng/winbar.nvim',
+--   config = function()
+--     require('pypeaday.plugins.winbar')
+--   end,
+--   requires = {
+--     "SmiteshP/nvim-navic",
+--     "neovim/nvim-lspconfig",
+--     "nvim-tree/nvim-web-devicons"
+--   },
+-- }) 
 use {
   'lambdalisue/suda.vim'
 }
