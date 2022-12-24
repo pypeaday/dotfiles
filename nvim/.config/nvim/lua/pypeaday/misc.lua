a83418799fb74d5915fb7e5228ed61f3cfe39be5
Pypeaday <pypeaday@pm.me>
2022-12-24 14:23:49 -0600

    WIP - broken autocommands

--- nvim/.config/nvim/lua/pypeaday/misc.lua
+++ nvim/.config/nvim/lua/pypeaday/misc.lua
@@ -1,24 +1,24 @@
 
-vim.cmd([[
-    function! s:PyPostSave()
-        execute 'silent !$HOME/.local/bin/tidy-imports --quiet --replace-star-imports --action REPLACE ' . bufname("%")
-        execute 'silent !isort ' . bufname("%")
-        execute 'silent !black ' . bufname("%")
-        execute 'silent lua vim.diagnostic.setloclist({open=false})'
-    endfunction
+local PyPostSave = function()
+    -- Get the filename of the current buffer
+    local filename = vim.fn.expand("%:p")
+    vim.api.nvim_command(string.format(":silent $HOME/.local/bin/tidy-imports --quiet --replace-star-imports --action REPLACE  %s", filename))
+    vim.api.nvim_command(string.format(":silent isort  %s", filename))
+    vim.api.nvim_command(string.format(":silent black %s", filename))
+    vim.diagnostic.setloclist({open=true})
+end
 
-    ":command! PyPreSave :call s:PyPreSave()
-    :command! PyPostSave :call s:PyPostSave()
+vim.api.nvim_command("autocmd BufWritePost *.py lua PyPostSave()")
 
-    augroup pypeaday
-        autocmd!
-        " remove trailing whitespace before python autocmd to keep newline at end of file
-        " autocmd bufwritepost * :%s/\s\+$//e
-        "-- autocmd BufWritePost *.py call flake8#Flake8()
-        autocmd bufwritepost *.py execute 'PyPostSave'
-        autocmd bufwritepost .tmux.conf execute ':!tmux source-file %'
-        autocmd bufwritepost .tmux.local.conf execute ':!tmux source-file %'
-        autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()
-        autocmd TextChanged,TextChangedI *qutebrowser-editor* silent write
-    augroup end
-]])
+-- vim.api.nvim_command(
+--     augroup pypeaday
+--         autocmd!
+--         " remove trailing whitespace before python autocmd to keep newline at end of file
+--         " autocmd bufwritepost * :%s/\s\+$//e
+--         autocmd bufwritepost *.py execute 'lua PyPostSave()'
+--         autocmd bufwritepost .tmux.conf execute ':!tmux source-file %'
+--         autocmd bufwritepost .tmux.local.conf execute ':!tmux source-file %'
+--         autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()
+--         autocmd TextChanged,TextChangedI *qutebrowser-editor* silent write
+--     augroup end
+-- )
