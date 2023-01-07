a841a1ab8c580ac2104f22be7545a6c57a0599f7
Pypeaday <32268273+pypeaday@users.noreply.github.com>
2023-01-06 20:29:02 -0600

    Bringing in changes from home and such (#9)
    * make starship more telling for ssh sessions
    
    * starship prompt hostname only in ssh sessions
    
    * always show user in starship prompt
    
    * idk why I can't have this in here but I don't need it now nayways
    
    * remove UltSnippets stuf
    
    * remove commented out plugins
    
    * remove airline.lua
    
    * silence isort
    
    * autocmd for prisma schemas
    
    * reformat for trying to get luasnippets to work
    
    * get snippets working!
    
    * isort is dead to me

--- nvim/.config/nvim/lua/pypeaday/misc.lua
+++ nvim/.config/nvim/lua/pypeaday/misc.lua
@@ -2,9 +2,7 @@
 vim.cmd([[
     function! s:PyPostSave()
         execute 'silent !$HOME/.local/bin/tidy-imports --quiet --replace-star-imports --action REPLACE --separate-from-imports ' . bufname("%")
-        " -lbt is --lines-between-types which defualts to 0. Change to 1 to be
-        " in line with Anthony's reorder_python_imports pre-commit hook
-        execute 'silent !isort -lbt 1' . bufname("%")
+        execute 'silent !reorder-python-imports ' . bufname("%")
         execute 'silent !black ' . bufname("%")
         execute 'silent lua vim.diagnostic.setloclist({open=false})'
     endfunction
@@ -20,6 +18,7 @@ vim.cmd([[
         autocmd bufwritepost *.py execute 'PyPostSave'
         "autocmd bufwritepost .tmux.conf execute ':!tmux source-file %'
         "autocmd bufwritepost .tmux.local.conf execute ':!tmux source-file %'
+        autocmd bufwritepost *.prisma execute ':! prisma validate'
         autocmd BufWritePre *.tf lua vim.lsp.buf.formatting_sync()
         autocmd TextChanged,TextChangedI *qutebrowser-editor* silent write
     augroup end
--- nvim/.config/nvim/lua/pypeaday/plugins/airline.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd([[
-    let g:airline_theme="night_owl"
-    let g:airline_powerline_fonts = 1
-    " Disable probably if using winbar
-    let g:airline#extensions#tabline#enabled = 1  
-    let g:airline#extensions#tabline#left_sep = ' '
-    let g:airline#extensions#tabline#left_alt_sep = '|'
-  ]])
