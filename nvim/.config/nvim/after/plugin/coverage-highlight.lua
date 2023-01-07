f48c959d15e6c085ef228fa86877d560c72b2426
pypeaday <pypeaday@pype.dev>
2023-01-07 13:41:58 -0600

    remap in coverage

--- nvim/.config/nvim/after/plugin/coverage-highlight.lua
+++ nvim/.config/nvim/after/plugin/coverage-highlight.lua
@@ -1,5 +1,8 @@
-local nnoremap = require('pypeaday.keymap_function').nnoremap
-local noremap = require('pypeaday.keymap_function').noremap
-noremap("[C", "<cmd><C-U>PrevUncovered<CR>") 
-noremap("]C", "<cmd><C-U>NextUncovered<CR>")  
-nnoremap("<leader>c", "<cmd>ToggleCoverage<CR>")  
+-- local nnoremap = require('pypeaday.keymap_function').nnoremap
+-- local noremap = require('pypeaday.keymap_function').noremap
+-- noremap("[c", "<cmd><C-U>PrevUncovered<CR>") 
+-- noremap("]C", "<cmd><C-U>NextUncovered<CR>")  
+-- nnoremap("<leader>c", "<cmd>ToggleCoverage<CR>")  
+vim.keymap.set('n', '[c', "<cmd><C-U>PrevUncovered<CR>", {noremap = false})
+vim.keymap.set('n', ']c', "<cmd><C-U>NextUncovered<CR>", {noremap = false})
+vim.keymap.set('n', '<leader>c', "<cmd>ToggleCoverage<CR>", {noremap = true})
