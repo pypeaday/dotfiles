32367f638c77aec1cc956f9afda48cf0f9ca1916
pypeaday <pypeaday@pype.dev>
2023-01-08 13:59:47 -0600

    add lspkind add lspkind

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -1,6 +1,11 @@
   -- Setup nvim-cmp.
 local cmp = require'cmp'
--- local lspkind = require('lspkind')
+local lspkind = require('lspkind')
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+end
 
 cmp.setup({
   snippet = {
@@ -40,23 +45,13 @@ cmp.setup({
     { name = 'spell' },
     -- { name = 'cmp_tabnine' }
     }),
-  -- formatting = {
-  --   format = lspkind.cmp_format({with_text = false, maxwidth = 50})
-  -- },
-    -- formatting = {
-    --         format = function(entry, vim_item)
-    --             vim_item.kind = lspkind.presets.default[vim_item.kind]
-    --             local menu = source_mapping[entry.source.name]
-    --             if entry.source.name == 'cmp_tabnine' then
-    --                 if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-    --                     menu = entry.completion_item.data.detail .. ' ' .. menu
-    --                 end
-    --                 vim_item.kind = ''
-    --             end
-    --             vim_item.menu = menu
-    --             return vim_item
-    --         end
-    --     },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+    })
+  },
 })
 
 -- spelling
