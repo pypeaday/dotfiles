939eb929124745b6b11415c136cad4d3050b0067
Pypeaday <32268273+pypeaday@users.noreply.github.com>
2023-01-09 11:29:20 -0600

    Updates mainly for LSP configuration (#16)
    * add tripple-monitor script
    
    * make install script a tiny bit better
    
    * remove old script
    
    * move trip monitor to work folder
    
    * icons for lsp
    
    * move ultest deprecation notice silence to init.lua
    
    * add back a little lspsage... not sure if I'll keep it
    
    * add lspkind
    add lspkind
    
    * move all cmp to lspconfig
    
    * need lspkind in plugins
    
    * Work (#12)
    
    * add tripple-monitor script
    
    * make install script a tiny bit better
    
    * remove old script
    
    * move trip monitor to work folder
    
    * icons for lsp
    
    * move ultest deprecation notice silence to init.lua
    
    * add back a little lspsage... not sure if I'll keep it
    
    * add lspkind
    add lspkind
    
    * move all cmp to lspconfig
    
    * need lspkind in plugins
    
    * Home (#13)
    
    * dedent block - only formatting
    
    * configure pylsp with flake8
    
    * formatting error
    
    * what keeps happening??

--- nvim/.config/nvim/lua/pypeaday/plugins.lua
+++ nvim/.config/nvim/lua/pypeaday/plugins.lua
@@ -129,31 +129,36 @@ use({
   end,
 })
 
- use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  requires = {
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+use {'onsails/lspkind.nvim'}
 
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
+use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+        -- LSP Support
+        {'neovim/nvim-lspconfig'},
+        {'williamboman/mason.nvim'},
+        {'williamboman/mason-lspconfig.nvim'},
 
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'},
-	  }
-  }
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'saadparwaiz1/cmp_luasnip'},
+        {'hrsh7th/cmp-nvim-lsp'},
+        {'hrsh7th/cmp-nvim-lua'},
 
--- use({
---   'glepnir/lspsaga.nvim',
--- })
+        -- Snippets
+        {'L3MON4D3/LuaSnip'},
+        {'rafamadriz/friendly-snippets'},
+    },
+}
+
+use({
+  'glepnir/lspsaga.nvim',
+  -- config = function()
+  --   require('pypeaday.lspsaga')
+  -- end,
+})
 
 use({'itchyny/vim-gitbranch'})
 
