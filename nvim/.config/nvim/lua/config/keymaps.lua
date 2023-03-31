711dd67acb86df3a66f956fafad7bf1bf427ac24
pypeaday <pypeaday@pype.dev>
2023-03-31 11:43:15 -0500

    bring my telescope keymaps into keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -149,6 +149,18 @@ nnoremap("<leader><leader>vd", "<cmd>lua require'telegraph'.telegraph({cmd='tmux
 vim.cmd(":command Format :%!jq .")
 vim.cmd(":command Unformat :%!jq -c .")
 
+-- Telescope
+
+nnoremap("gen", "<cmd>Telescope find_files cwd=~/.config/nvim <CR>")
+nnoremap("<leader>ps", "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep For > ')})<CR>")
+nnoremap("<Leader>pf",
+    "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files','--no-ignore', '--hidden',  '--iglob', '!.venv','-g' ,'!.git' }})<cr>")
+nnoremap("<Leader>pg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+nnoremap("<leader>pw", "<cmd>lua require('telescope.builtin').grep_string { search =  vim.fn.expand(\"<cword>\") }<CR>")
+nnoremap("<leader>pb", "<cmd>lua require('telescope.builtin').buffers()<CR>")
+nnoremap("<leader>pl", "<cmd>lua require('telescope.builtin').loclist()<CR>")
+nnoremap("<leader>ch", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
+
 --
 -- -- lsp
 -- vim.keymap.set("i", "jk", "<esc>", { desc = "Exit Insert Mode with jk" })
