f268be5618c83295cc149bc497946d16e5a126ce
pypeaday <pypeaday@pype.dev>
2023-03-30 21:52:18 -0500

    tokyonight wth transparency

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      style = "moon",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
