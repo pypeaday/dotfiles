691e14a95ea03bfbbe1564fb9bc2ef8bdffcc572
pypeaday <pypeaday@pype.dev>
2023-03-31 16:29:49 -0500

    WIP: winbar just will not work...

---
+++ nvim/.config/nvim/lua/plugins/winbar.lua
@@ -0,0 +1,11 @@
+return {
+  -- "fgheng/winbar.nvim",
+  -- dependencies = {
+  --   "SmiteshP/nvim-navic",
+  --   "neovim/nvim-lspconfig",
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
+  -- config = function()
+  --   require("winbar").setup()
+  -- end,
+}
