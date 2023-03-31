4ce815daf1a4333020f2a25628cc891e99143b81
pypeaday <pypeaday@pype.dev>
2023-03-31 11:45:06 -0500

    remove a bunch of stuff

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -58,20 +58,4 @@ return {
       })
     end
   },
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   event = "BufReadPre",
-  --   dependencies = {
-  --     { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-  --     { "folke/neodev.nvim", config = true },
-  --     { "j-hui/fidget.nvim", config = true },
-  --     { "smjonas/inc-rename.nvim", config = true },
-  --     "simrat39/rust-tools.nvim",
-  --     "rust-lang/rust.vim",
-  --     "williamboman/mason.nvim",
-  --     "williamboman/mason-lspconfig.nvim",
-  --     "hrsh7th/cmp-nvim-lsp",
-  --     "hrsh7th/cmp-nvim-lsp-signature-help",
-  --   },
-  -- },
 }
