017f3f772321c30805f84e8707f34e8e2f69e6e6
pypeaday <pypeaday@pype.dev>
2023-03-30 20:48:23 -0500

    delete backup so I can search more nicely as I try out lazy

--- nvim/.config/nvim-backup/after/plugin/coverage-highlight.lua
+++
@@ -1,8 +0,0 @@
--- local nnoremap = require('pypeaday.keymap_function').nnoremap
--- local noremap = require('pypeaday.keymap_function').noremap
--- noremap("[c", "<cmd><C-U>PrevUncovered<CR>") 
--- noremap("]C", "<cmd><C-U>NextUncovered<CR>")  
--- nnoremap("<leader>c", "<cmd>ToggleCoverage<CR>")  
-vim.keymap.set('n', '[c', "<cmd><C-U>PrevUncovered<CR>", {noremap = false})
-vim.keymap.set('n', ']c', "<cmd><C-U>NextUncovered<CR>", {noremap = false})
-vim.keymap.set('n', '<leader>c', "<cmd>ToggleCoverage<CR>", {noremap = true})
