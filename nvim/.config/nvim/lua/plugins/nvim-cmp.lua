f7c90600c0f90bc7b38c7e7c035a9f01491a882a
pypeaday <pypeaday@pype.dev>
2023-03-31 10:56:48 -0500

    windows around cmp results!

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -20,6 +20,10 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
         mapping = cmp.mapping.preset.insert({
           -- disable completion with tab
           -- this helps with copilot setup
