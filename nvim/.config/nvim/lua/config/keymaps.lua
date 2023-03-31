f0887bcba8c64d4081ca7ba9be57897421e0ad11
pypeaday <pypeaday@pype.dev>
2023-03-31 06:45:46 -0500

    fix typo for noremap in keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -10,29 +10,29 @@ vim.keymap.set(
   "n",
   "<silent> (( ",
   "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>",
-  { nnoremap = true, desc = "Go to prev" }
+  { noremap = true, desc = "Go to prev" }
 )
 vim.keymap.set(
   "n",
   "<silent> )) ",
   "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
-  { nnoremap = true, desc = "Go to next" }
+  { noremap = true, desc = "Go to next" }
 )
-vim.keymap.set("n", "<leader>vd", "<cmd>lua vim.lsp.buf.definition()<CR>", { nnoremap = true, desc = "" })
-vim.keymap.set("n", "<leader>vD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { nnoremap = true, desc = "" })
-vim.keymap.set("n", "<leader>vh", "<cmd>lua vim.lsp.buf.hover()<CR>", { nnoremap = true, desc = "" })
-vim.keymap.set("n", "<leader>vi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { nnoremap = true, desc = "" })
-vim.keymap.set("n", "<leader>vsh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { nnoremap = true, desc = "" })
--- nnoremap("<leader>vrr", "<cmd>lua vim.lsp.buf.references()<CR>")
-vim.keymap.set("n", "<leader>vrr", ":Telescope lsp_references<CR>", { nnoremap = true, desc = "" })
-vim.keymap.set("n", "<leader>vrn", "<cmd>lua vim.lsp.buf.rename()<CR>", { nnoremap = true, desc = "" })
-vim.keymap.set("n", "<leader>vca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { nnoremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vh", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vsh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, desc = "" })
+-- noremap("<leader>vrr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "<leader>vrr", ":Telescope lsp_references<CR>", { noremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vrn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, desc = "" })
 -- " show_line_diagnostics deprecated for open_float
-vim.keymap.set("n", "<leader>vsd", " vim.diagnostic.open_float()<CR>  ", { nnoremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vsd", " vim.diagnostic.open_float()<CR>  ", { noremap = true, desc = "" })
 vim.keymap.set(
   "n",
   "<leader>vsl",
   "<cmd> lua vim.diagnostic.setloclist({open=false})<CR>",
-  { nnoremap = true, desc = "" }
+  { noremap = true, desc = "" }
 )
-vim.keymap.set("n", "<leader>vn", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { nnoremap = true, desc = "" })
+vim.keymap.set("n", "<leader>vn", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { noremap = true, desc = "" })
