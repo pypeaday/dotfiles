27dd291c51ea2bef058809d17718627214804b7d
pypeaday <pypeaday@pype.dev>
2023-03-31 12:24:11 -0500

    add vimtek

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -30,3 +30,20 @@ vim.cmd([[
 ]])
 
 vim.g.ultest_use_pty = 1
+
+-- vimtek
+vim.g.vimtex_view_general_viewer = "okular"
+vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
+vim.g.vimtex_view_general_options_latexmk = "--unique"
+
+-- VimTeX uses latexmk as the default compiler backend. If you use it, which is
+-- strongly recommended, you probably don't need to configure anything. If you
+-- want another compiler backend, you can change it as follows. The list of
+-- supported backends and further explanation is provided in the documentation,
+-- see ":help vimtex-compiler".
+vim.g.vimtex_compiler_method = "latexrun"
+vim.g.vimtex_compiler_method = "latexmk"
+
+-- Most VimTeX mappings rely on localleader and this can be changed with the
+-- following line. The default is usually fine and is the symbol "\".
+vim.maplocalleader = ","
