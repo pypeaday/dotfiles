7e7e29ba2d4ed37a2dcb72d6061de6137317f6db
pypeaday <pypeaday@pype.dev>
2023-03-31 09:00:30 -0500

    Revert "re-delete all my old nvim stuff"
    This reverts commit 3d36e8ea1815f562efd6e4b9a742213bff758ae8.

---
+++ nvim/.config/nvim-backup/after/plugin/cloak.lua
@@ -0,0 +1,14 @@
+require('cloak').setup({
+  enabled = true,
+  patterns = {
+    {
+      -- Match any file starting with '.yml'.
+      file_pattern = {
+        'credentials.yml',
+        '.env'
+      },
+      -- Match a con: sign and any character after it.
+      cloak_pattern = ':.+'
+    },
+  },
+})
