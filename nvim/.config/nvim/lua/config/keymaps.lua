165bec9c4a690ef16adcc6a371fa71db48aaaca3
pypeaday <github@pype.aleeas.com>
2023-04-11 08:09:32 -0500

    I disable Folke's default keymap and copied it into my own because I NEED gww BACK

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -15,7 +15,6 @@ local function map(mode, lhs, rhs, opts)
   end
 end
 
-local M = {}
 local function bind(op, outer_opts)
   outer_opts = outer_opts or { noremap = true }
   return function(lhs, rhs, opts)
@@ -25,17 +24,16 @@ local function bind(op, outer_opts)
   end
 end
 
-nmap = bind("n", { noremap = false })
-nnoremap = bind("n")
-vnoremap = bind("v")
-xnoremap = bind("x")
-inoremap = bind("i")
+local nmap = bind("n", { noremap = false })
+local nnoremap = bind("n")
+local vnoremap = bind("v")
+local xnoremap = bind("x")
+local inoremap = bind("i")
 
 -- " Behave Vim
 --
 nnoremap("Y", "y$")
-vnoremap("<", "<gv")
-vnoremap(">", ">gv")
+
 -- " yank text to first regiter then when you paste it the yanked text is still first in the register
 -- " vnoremap <Leader>p "_P
 vnoremap("<leader>p", '"0p')
@@ -57,37 +55,11 @@ inoremap(".", ".<c-g>u")
 inoremap("!", "!<c-g>u")
 inoremap("?", "?<c-g>u")
 inoremap("[", "[<c-g>u")
+inoremap(";", "[<c-g>u")
 
 -- " Jumplist
 nnoremap("<expr> k", "(v:count > 5 ? 'm'' . v:count : '') . 'k' ")
 nnoremap("<expr> j", "(v:count > 5 ? 'm'' . v:count : '') . 'j' ")
-
--- " quickfix
-
--- local toggleQF = vim.api.nvim_exec([[
-
---     let s:cisopen = 0
---     function! s:ToggleQuickFix()
-
---         if s:cisopen  == 1
---             let s:cisopen = 0
---             :bel copen
---             :wincmd k
-
---         else
---             let s:cisopen = 1
---             :cclose
---         endif
---     endfunction
-
---     call s:ToggleQuickFix()
---     ]],
---     true)
-
--- nnoremap("<Leader>qft", "<cmd>ToggleQuickFix<CR>")
--- nnoremap("<Leader>qfc", "<cmd>cexpr []<CR>")
--- nnoremap("<C-n>", "<cmd>cnext<CR>")
--- nnoremap("<C-p> ", "<cmd>cprev<CR>")
 
 -- " Moving text
 vnoremap("J", "<cmd>m '>+1<CR>gv=gv")
@@ -95,22 +67,8 @@ vnoremap("K", "<cmd>m '<-2<CR>gv=gv")
 inoremap("<C-j>", "<esc><cmd>m .+1<CR>==")
 inoremap("<C-k>", "<esc><cmd>m .-2<CR>==")
 
--- " split navigations
--- nnoremap("<C-K>", "<C-W><C-K>")
--- nnoremap("<C-J>", "<C-W><C-J>")
--- nnoremap("<C-L>", "<C-W><C-L>")
--- nnoremap("<C-H>", "<C-W><C-H>")
-
 -- " copy to clipbord
 vnoremap("<Leader>y", ' "+y')
-
--- " edit nvim dotfiles
-nnoremap("gek", "<cmd>e ~/.config/nvim/lua/pypeaday/keymap.lua<CR>")
-nnoremap("gel", "<cmd>e ~/.config/nvim/lua/pypeaday/plugins/lspconfig.lua<CR>")
-nnoremap("gep", "<cmd>e ~/.config/nvim/lua/pypeaday/plugins.lua<CR>")
-nnoremap("ges", "<cmd>e ~/.config/nvim/lua/pypeaday/settings.lua<CR>")
-nnoremap("geo", "<cmd>e ~/.config/nvim/lua/pypeaday/options.lua<CR>")
-nnoremap("gem", "<cmd>e ~/.config/nvim/lua/pypeaday/misc.lua<CR>")
 
 nnoremap("get", "<cmd>e ~/.tmux.conf.local<CR>")
 nnoremap("gez", "<cmd>e ~/.zshrc<CR>")
@@ -126,8 +84,6 @@ nnoremap("[C", "<cmd><C-U>PrevUncovered<CR>")
 nnoremap("]C", "<cmd><C-U>NextUncovered<CR>")
 nnoremap("<leader>c", "<cmd>ToggleCoverage<CR>")
 
--- " Testing things
--- "――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 -- " Work
 nnoremap("gec", "<cmd>e ~/.caterpillar/credentials.yml<CR>")
 -- " nnoremap <leader><leader>vd <cmd>lua require'telegraph'.telegraph({cmd='pipx run visidata {cWORD}', how='tmux'})<CR>
@@ -137,9 +93,6 @@ nnoremap(
 )
 -- " nnoremap <leader><leader>vd <cmd>lua require'telegraph'.telegraph({cmd='visidata {cWORD}', how='tmux_popup'})<CR>
 --
--- might be unnecessary with neoformat
-vim.cmd(":command Format :%!jq .")
-vim.cmd(":command Unformat :%!jq -c .")
 
 -- Telescope
 
@@ -155,39 +108,6 @@ nnoremap("<leader>pb", "<cmd>lua require('telescope.builtin').buffers()<CR>")
 nnoremap("<leader>pl", "<cmd>lua require('telescope.builtin').loclist()<CR>")
 nnoremap("<leader>ch", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
 
---
--- I'm not using any of these anymore - trying to use whatever is in Lazyvim
--- vim.keymap.set(
---   "n",
---   "<silent> (( ",
---   "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>",
---   { noremap = true, desc = "Go to prev" }
--- )
--- vim.keymap.set(
---   "n",
---   "<silent> )) ",
---   "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
---   { noremap = true, desc = "Go to next" }
--- )
--- vim.keymap.set("n", "<leader>vd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, desc = "" })
--- vim.keymap.set("n", "<leader>vD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, desc = "" })
--- vim.keymap.set("n", "<leader>vh", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, desc = "" })
--- vim.keymap.set("n", "<leader>vi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, desc = "" })
--- vim.keymap.set("n", "<leader>vsh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, desc = "" })
--- -- noremap("<leader>vrr", "<cmd>lua vim.lsp.buf.references()<CR>")
--- vim.keymap.set("n", "<leader>vrr", ":Telescope lsp_references<CR>", { noremap = true, desc = "" })
--- vim.keymap.set("n", "<leader>vrn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, desc = "" })
--- vim.keymap.set("n", "<leader>vca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true, desc = "" })
--- -- " show_line_diagnostics deprecated for open_float
--- vim.keymap.set("n", "<leader>vsd", " vim.diagnostic.open_float()<CR>  ", { noremap = true, desc = "" })
--- vim.keymap.set(
---   "n",
---   "<leader>vsl",
---   "<cmd> lua vim.diagnostic.setloclist({open=false})<CR>",
---   { noremap = true, desc = "" }
--- )
--- vim.keymap.set("n", "<leader>vn", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { noremap = true, desc = "" })
---
 -- Ultest, Neotest, ...
 
 nnoremap("<leader>tn", "<cmd>UltestNearest<CR>")
@@ -216,3 +136,136 @@ keymap("n", "<leader>pd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
 
 -- Outline
 keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>", { silent = true })
+
+-- These settings are LazyVim default but I just hate the gw remap so I disable loading Folke's and I just copy the rest of his default keymaps here
+-- better up/down
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- buffers
+if Util.has("bufferline.nvim") then
+  map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+  map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+  map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+  map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+else
+  map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+  map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+  map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
+  map("n", "]b", "<cmd>bnext<cr>", { desc = "Next buffer" })
+end
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+map(
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / clear hlsearch / diff update" }
+)
+
+-- Sorry folke but I need my gww
+-- map({ "n", "x" }, "gw", "*N", { desc = "Search word under cursor" })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+
+-- Add undo break-points
+map("i", ",", ",<c-g>u")
+map("i", ".", ".<c-g>u")
+map("i", ";", ";<c-g>u")
+
+-- save file
+map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- lazy
+map("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
+
+-- new file
+map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+
+map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
+map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
+
+if not Util.has("trouble.nvim") then
+  map("n", "[q", vim.cmd.cprev, { desc = "Previous quickfix" })
+  map("n", "]q", vim.cmd.cnext, { desc = "Next quickfix" })
+end
+
+-- stylua: ignore start
+
+-- toggle options
+map("n", "<leader>uf", require("lazyvim.plugins.lsp.format").toggle, { desc = "Toggle format on Save" })
+map("n", "<leader>us", function() Util.toggle("spell") end, { desc = "Toggle Spelling" })
+map("n", "<leader>uw", function() Util.toggle("wrap") end, { desc = "Toggle Word Wrap" })
+map("n", "<leader>ul", function() Util.toggle("relativenumber", true) Util.toggle("number") end, { desc = "Toggle Line Numbers" })
+map("n", "<leader>ud", Util.toggle_diagnostics, { desc = "Toggle Diagnostics" })
+local conceallevel = vim.o.conceallevel > 0 and vim.o.conceallevel or 3
+map("n", "<leader>uc", function() Util.toggle("conceallevel", false, {0, conceallevel}) end, { desc = "Toggle Conceal" })
+
+-- lazygit
+map("n", "<leader>gg", function() Util.float_term({ "lazygit" }, { cwd = Util.get_root() }) end, { desc = "Lazygit (root dir)" })
+map("n", "<leader>gG", function() Util.float_term({ "lazygit" }) end, { desc = "Lazygit (cwd)" })
+
+-- quit
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- highlights under cursor
+if vim.fn.has("nvim-0.9.0") == 1 then
+  map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
+end
+
+-- floating terminal
+map("n", "<leader>ft", function() Util.float_term(nil, { cwd = Util.get_root() }) end, { desc = "Terminal (root dir)" })
+map("n", "<leader>fT", function() Util.float_term() end, { desc = "Terminal (cwd)" })
+map("t", "<esc><esc>", "<c-\\><c-n>", {desc = "Enter Normal Mode"})
+
+-- windows
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window" })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete window" })
+map("n", "<leader>w-", "<C-W>s", { desc = "Split window below" })
+map("n", "<leader>w|", "<C-W>v", { desc = "Split window right" })
+map("n", "<leader>-", "<C-W>s", { desc = "Split window below" })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right" })
+
+-- tabs
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
---
+++ nvim/.config/nvim/lua/plugins/lazyvim.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      defaults = {
+        autocmds = true, -- lazyvim.config.autocmds
+        -- turning off keymaps to work on gww
+        keymaps = false, -- lazyvim.config.keymaps
+        options = true, -- lazyvim.config.options
+      },
+    },
+  },
+}
