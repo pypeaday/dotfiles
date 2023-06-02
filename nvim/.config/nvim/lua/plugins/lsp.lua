b37890944d9788ca715f8d72716c101ff9f69e9e
pypeaday <github@pype.aleeas.com>
2023-06-02 13:38:59 +0000

    WIP: try leaning more heavily into the example config from lazyVim

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,62 @@
+if true then
+  return {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pylsp = {
+          -- configurationSources = { "flake8" },
+          enabled = { true },
+          plugins = {
+            ruff = {
+              enabled = true,
+              extendSelect = { "I" },
+              extendIgnore = { "E501" },
+            },
+            pyflakes = { enabled = false },
+            pycodestyle = { enabled = false },
+            flake8 = { enabled = false },
+            mypy = {
+              enabled = true,
+              live_mode = true,
+              strict = true,
+            },
+          },
+        },
+        -- X will be automatically installed with mason and loaded with lspconfig
+        -- pyright = {},
+        ruff_lsp = {},
+        -- jedi_language_server = {},
+        dockerls = {},
+        bashls = {},
+        yamlls = {
+          settings = {
+            yamlVersion = 1.2,
+            keyOrdering = false,
+            validate = false,
+          },
+          -- enabled = { false },
+          schemas = {
+            ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"] = "conf/**/*catalog*",
+            ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+            ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = {
+              "/azure-pipeline*.y*l",
+              "/*.azure*",
+              "Azure-Pipelines/**/*.y*l",
+              "Pipelines/*.y*l",
+            },
+          },
+        },
+        jsonls = {},
+        html = {},
+        terraformls = {},
+        marksman = {},
+      },
+    },
+  }
+end
+-- LSP Zero not working right...
 return {
   {
     "VonHeikemen/lsp-zero.nvim",
@@ -7,9 +66,9 @@ return {
       { "neovim/nvim-lspconfig" }, -- Required
       { -- Optional
         "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
+        -- build = function()
+        --   pcall(vim.cmd, "MasonUpdate")
+        -- end,
       },
       { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
