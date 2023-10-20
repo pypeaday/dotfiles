e86d2e815568133b08d28e3c6a94f7707cb24a76
pypeaday <github@pype.aleeas.com>
2023-10-20 12:04:15 -0500

    disable luasnip keys

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -4,6 +4,10 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
     },
+    -- disable keys for supertab
+    keys = function()
+      return {}
+    end,
     opts = function()
       require("luasnip.loaders.from_snipmate").lazy_load({})
     end,
