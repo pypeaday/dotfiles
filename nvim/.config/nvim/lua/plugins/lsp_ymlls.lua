74c7dcc4854acd8fd5c5853b6d8bb7362779a835
pypeaday <github@pype.aleeas.com>
2023-10-20 12:36:52 -0500

    formatting but ymlls autocomplete still isn't working

--- nvim/.config/nvim/lua/plugins/lsp_ymlls.lua
+++ nvim/.config/nvim/lua/plugins/lsp_ymlls.lua
@@ -25,8 +25,10 @@ return {
           settings = {
             schemas = {
               -- GH Actions
-              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-              ["/Users/npayne81/dotfiles/avant/.avant-hidden-though-stowed/github-workflow.json"] = "/.github/workflows/*",
+              -- ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+              ["/Users/npayne81/dotfiles/avant/.avant-hidden-though-stowed/github-workflow.json"] = {
+                "/.github/workflows/*",
+              },
               -- DBT stuff
               ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_yml_files.json"] = {
                 "/staging/**/*.yml",
@@ -48,14 +50,15 @@ return {
                 "packages.yml",
               },
             },
-            ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-            ["http://json.schemastore.org/ansible-playbook"] = "*ansible*.{yml,yaml}",
-            ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-            ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-            ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-            ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-            ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-            ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            ["http://json.schemastore.org/ansible-stable-2.9"] = { "roles/tasks/*.{yml,yaml}" },
+            ["http://json.schemastore.org/ansible-playbook"] = { "*ansible*.{yml,yaml}" },
+            ["http://json.schemastore.org/chart"] = { "Chart.{yml,yaml}" },
+            ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = {
+              "*docker-compose*.{yml,yaml}",
+            },
+            ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = {
+              "*flow*.{yml,yaml}",
+            },
           },
         },
       },
