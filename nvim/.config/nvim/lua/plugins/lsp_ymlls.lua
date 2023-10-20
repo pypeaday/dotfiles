b4f0839dc5a45d11ebaa7d2ba887483191325626
pypeaday <github@pype.aleeas.com>
2023-10-20 12:04:11 -0500

    try disabling schemastore, and using ymlls builtin

--- nvim/.config/nvim/lua/plugins/lsp_ymlls.lua
+++ nvim/.config/nvim/lua/plugins/lsp_ymlls.lua
@@ -14,80 +14,52 @@ return {
               },
             },
           },
-          -- lazy-load schemastore when needed
-          on_new_config = function(new_config)
-            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
-              "force",
-              new_config.settings.yaml.schemas or {},
-              require("schemastore").yaml.schemas()
-            )
-          end,
+          -- -- lazy-load schemastore when needed
+          -- on_new_config = function(new_config)
+          --   new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+          --     "force",
+          --     new_config.settings.yaml.schemas or {},
+          --     require("schemastore").yaml.schemas()
+          --   )
+          -- end,
           settings = {
-            redhat = { telemetry = { enabled = false } },
-            yaml = {
-              keyOrdering = false,
-              format = {
-                enable = true,
+            schemas = {
+              -- GH Actions
+              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+              ["/Users/npayne81/dotfiles/avant/.avant-hidden-though-stowed/github-workflow.json"] = "/.github/workflows/*",
+              -- DBT stuff
+              ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_yml_files.json"] = {
+                "/staging/**/*.yml",
+                "/intermediate/**/*.yml",
+                "/marts/**/*.yml",
+                "!profiles.yml",
+                "!dbt_project.yml",
+                "!packages.yml",
+                "!selectors.yml",
+                "!profile_template.yml",
               },
-              validate = true,
-              schemaStore = {
-                -- Must disable built-in schemaStore support to use
-                -- schemas from SchemaStore.nvim plugin
-                enable = false,
-                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-                url = "",
+              ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_project.json"] = {
+                "dbt_project.yml",
               },
-              schemas = vim.list_extend(
-                {
-                  -- DBT stuff
-                  ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_yml_files.json"] = {
-                    "/staging/**/*.yml",
-                    "/intermediate/**/*.yml",
-                    "/marts/**/*.yml",
-                    "!profiles.yml",
-                    "!dbt_project.yml",
-                    "!packages.yml",
-                    "!selectors.yml",
-                    "!profile_template.yml",
-                  },
-                  ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/dbt_project.json"] = {
-                    "dbt_project.yml",
-                  },
-                  ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/selectors.json"] = {
-                    "selectors.yml",
-                  },
-                  ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/packages.json"] = {
-                    "packages.yml",
-                  },
-                },
-                require("schemastore").yaml.schemas({
-                  ignore = { "GitHub Workflow" },
-                  -- replace = {
-                  --   ["GitHub Workflow"] = {
-                  --     name = "GitHub Workflow",
-                  --     description = "Avant schema for YAML GitHub Workflow",
-                  --     fileMatch = { "**/.github/workflows/*.y*ml" },
-                  --     url = "file:///Users/npayne81/dotfiles/avant/.avant-hidden-though-stowed/github-workflow.json",
-                  --   },
-                  -- },
-                })
-              ),
+              ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/selectors.json"] = {
+                "selectors.yml",
+              },
+              ["https://raw.githubusercontent.com/dbt-labs/dbt-jsonschema/main/schemas/packages.json"] = {
+                "packages.yml",
+              },
             },
+            ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+            ["http://json.schemastore.org/ansible-playbook"] = "*ansible*.{yml,yaml}",
+            ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+            ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+            ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+            ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+            ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+            ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
           },
         },
       },
-      setup = {
-        yamlls = function()
-          -- Neovim < 0.10 does not have dynamic registration for formatting
-          if vim.fn.has("nvim-0.10") == 0 then
-            require("lazyvim.util").lsp.on_attach(function(client, _)
-              if client.name == "yamlls" then
-                client.server_capabilities.documentFormattingProvider = true
-              end
-            end)
-          end
-        end,
-      },
     },
+    setup = {},
   },
 }
--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -8,9 +8,43 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-emoji",
     },
-    opts = function()
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+      local luasnip = require("luasnip")
       local cmp = require("cmp")
+      -- override nvim-cmp and add cmp-emoji
+      table.insert(opts.sources, { name = "emoji" })
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
       return {
         completion = {
           completeopt = "menu,menuone,noinsert",
@@ -27,9 +61,8 @@ return {
         mapping = cmp.mapping.preset.insert({
           -- disable completion with tab
           -- this helps with copilot setup
-          -- using tabby so I want to use Tab to complete
-          -- ["<Tab>"] = nil,
-          -- ["<S-Tab>"] = nil,
+          ["<Tab>"] = nil,
+          ["<S-Tab>"] = nil,
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -49,11 +82,11 @@ return {
           { name = "nvim_lsp_signature_help", priority = 1 },
           { name = "nvim_lsp", priority = 1 },
           { name = "luasnip", priority = 2 },
-          { name = "buffer", priority = 3 },
+          { name = "buffer", priority = 10 },
           { name = "path", priority = 4 },
           { name = "nvim_lua", priority = 5 },
-          { name = "treesitter", priority = 2 },
-          { name = "tmux", priority = 2 },
+          -- { name = "treesitter", priority = 2 },
+          -- { name = "tmux", priority = 2 },
           { name = "spell", priority = 3 },
           { name = "git", priority = 2 },
           { name = "rg", priority = 2 },
