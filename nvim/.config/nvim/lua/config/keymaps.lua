d734f653bab0dbaddd72f410aca84be925de2333
pypeaday <github@pype.aleeas.com>
2024-01-02 10:45:51 -0600

    different kepmap function

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,22 +4,24 @@
 
 local Util = require("lazyvim.util")
 
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
+local map = Util.safe_keymap_set
+
+-- local function map(mode, lhs, rhs, opts)
+--   local keys = require("lazy.core.handler").handlers.keys
+--   ---@cast keys LazyKeysHandler
+--   -- do not create the keymap if a lazy keys handler exists
+--   if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+--     opts = opts or {}
+--     opts.silent = opts.silent ~= false
+--     map(mode, lhs, rhs, opts)
+--   end
+-- end
 
 local function bind(op, outer_opts)
   outer_opts = outer_opts or { noremap = true }
   return function(lhs, rhs, opts)
     opts = vim.tbl_extend("force", outer_opts, opts or {})
-    vim.keymap.set(op, lhs, rhs, opts)
+    map(op, lhs, rhs, opts)
     -- vim.api.nvim_set_keymap(op, lhs, rhs, opts)
   end
 end
