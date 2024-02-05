f9e38a91012f2018094510af9ac5f5d67f7185ba
pypeaday <github@pype.aleeas.com>
2024-02-05 10:06:38 -0600

    update python host prog

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "~/dotfiles/.venv/nvim/bin/python"
+vim.g.python3_host_prog = "~/dotfiles/.venv/dotfiles/bin/python"
 vim.g.vim_markdown_folding_style_pythonic = { "python", "bash=sh", "json", "yaml" }
 vim.g.SimpylFold_docstring_preview = 1
 vim.g.indent_guides_enable_on_vim_startup = 1
